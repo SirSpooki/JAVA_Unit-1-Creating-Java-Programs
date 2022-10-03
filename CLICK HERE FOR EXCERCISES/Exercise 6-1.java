6728f047c7d5f24fa107be38d1b2f4c6015e106a
SirSpooki <114715053+SirSpooki@users.noreply.github.com>
2022-10-02 19:41:13 -0500

    Update and rename Exercise 6-1 to Exercise 6-1.java

--- CLICK HERE FOR EXCERCISES/Exercise 6-1.java
+++ CLICK HERE FOR EXCERCISES/Exercise 6-1.java
@@ -1,6 +1,13 @@
+
+/*
+    SirSpooki
+    SlayTeam 2023
+*/
+
 class SongLyrics {
     public static void main(String[] args) {
-        System.out.println("call me cree");
+       
+       System.out.println("call me cree");
         System.out.println("the way i shoot my shot");
     }
 }
