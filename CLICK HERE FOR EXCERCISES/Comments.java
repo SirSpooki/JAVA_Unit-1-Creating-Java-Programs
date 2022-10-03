cdfa8ed64d6a7f8c8b2b9e72e5e7d9c3de4a3dcf
SirSpooki <114715053+SirSpooki@users.noreply.github.com>
2022-10-02 19:52:47 -0500

    Add files via upload

---
+++ CLICK HERE FOR EXCERCISES/Comments.java
@@ -0,0 +1,10 @@
+class Comments {
+    public static void main(String[] args) {
+        System.out.println("Program comments are nonexecuting statements you add to a file for  documentation.");
+        //Program comments are nonexecuting statements you add to a file for documentation.
+        /*
+        Program comments are nonexecuting statements you add to a file for documentation.
+        */
+        /** Program comments are nonexecuting statements you add to a file for documentation.**/
+    }
+}
