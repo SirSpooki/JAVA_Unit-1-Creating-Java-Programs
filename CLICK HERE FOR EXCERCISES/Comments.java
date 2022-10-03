9616c41f3554443925bbe4a1125f17744b5ab17d
SirSpooki <114715053+SirSpooki@users.noreply.github.com>
2022-10-02 19:55:11 -0500

    Update Comments.java

--- CLICK HERE FOR EXCERCISES/Comments.java
+++ CLICK HERE FOR EXCERCISES/Comments.java
@@ -1,3 +1,7 @@
+/*
+    SirSpooki
+    SlayTeam 2023
+*/
 class Comments {
     public static void main(String[] args) {
         System.out.println("Program comments are nonexecuting statements you add to a file for  documentation.");
