396aaef176c8d2a0ef18f7416fa9a12ba3053265
SirSpooki <114715053+SirSpooki@users.noreply.github.com>
2022-10-02 20:06:30 -0500

    Create DebugOne1.java

---
+++ Click Here For Debugging/DebugOne1.java
@@ -0,0 +1,12 @@
+/*
+    SirSpooki
+    SlayTeam 2023
+*/
+public class DebugOne1
+{
+   /* This program displays a greeting*/
+   public static void main(String[] args)
+   {
+      System.out.println("Hello");
+   }
+}
